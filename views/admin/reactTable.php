e237ef7b0023c044b563b19306fd7ce99e8b3f31
Ahmed Hisham <129528003+AhmedSubway@users.noreply.github.com>
2023-12-09 21:21:28 +0200

    sub

--- views/admin/reactTable.php
+++ views/admin/reactTable.php
@@ -124,170 +124,34 @@
   <!---<div class="toolbar">
     <button id="alertBtn" class="btn btn-default">Alert</button>
   </div>--->
+  <?php
 
+      ?>
   <table id="fresh-table" class="table">
     
     <thead>
       <th data-field="id">ID</th>
-      <th data-field="name">Name</th>
-      <th data-field="salary">Salary</th>
-      <th data-field="country">Country</th>
-      <th data-field="city">City</th>
+      <th data-field="name">Username</th>
+      <th data-field="salary">Email</th>
+      <th data-field="country">UserRole</th>
+      <th data-field="city">PhoneNumber</th>
       <th data-field="actions" data-formatter="operateFormatter" data-events="operateEvents">Actions</th>
     </thead>
     <tbody>
-      <tr>
-        <td>1</td>
-        <td>Dakota Rice</td>
-        <td>$36,738</td>
-        <td>Niger</td>
-        <td>Oud-Turnhout</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>Minerva Hooper</td>
-        <td>$23,789</td>
-        <td>Curaçao</td>
-        <td>Sinaai-Waas</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td>Sage Rodriguez</td>
-        <td>$56,142</td>
-        <td>Netherlands</td>
-        <td>Baileux</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>4</td>
-        <td>Philip Chaney</td>
-        <td>$38,735</td>
-        <td>Korea, South</td>
-        <td>Overland Park</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>5</td>
-        <td>Doris Greene</td>
-        <td>$63,542</td>
-        <td>Malawi</td>
-        <td>Feldkirchen in Kärnten</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>6</td>
-        <td>Mason Porter</td>
-        <td>$78,615</td>
-        <td>Chile</td>
-        <td>Gloucester</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>7</td>
-        <td>Alden Chen</td>
-        <td>$63,929</td>
-        <td>Finland</td>
-        <td>Gary</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>8</td>
-        <td>Colton Hodges</td>
-        <td>$93,961</td>
-        <td>Nicaragua</td>
-        <td>Delft</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>9</td>
-        <td>Illana Nelson</td>
-        <td>$56,142</td>
-        <td>Heard Island</td>
-        <td>Montone</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>10</td>
-        <td>Nicole Lane</td>
-        <td>$93,148</td>
-        <td>Cayman Islands</td>
-        <td>Cottbus</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>11</td>
-        <td>Chaim Saunders</td>
-        <td>$5,502</td>
-        <td>Romania</td>
-        <td>New Quay</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>12</td>
-        <td>Josiah Simon</td>
-        <td>$50,265</td>
-        <td>Christmas Island</td>
-        <td>Sint-Jans-Molenbeek</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>13</td>
-        <td>Ila Poole</td>
-        <td>$67,413</td>
-        <td>Montenegro</td>
-        <td>Pontevedra</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>14</td>
-        <td>Shana Mejia</td>
-        <td>$58,566</td>
-        <td>Afghanistan</td>
-        <td>Ballarat</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>15</td>
-        <td>Lana Ryan</td>
-        <td>$64,151</td>
-        <td>Martinique</td>
-        <td>Portobuffolè</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>16</td>
-        <td>Daquan Bender</td>
-        <td>$91,906</td>
-        <td>Sao Tome and Principe</td>
-        <td>Walhain-Saint-Paul</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>17</td>
-        <td>Connor Wagner</td>
-        <td>$86,537</td>
-        <td>Germany</td>
-        <td>Solihull</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>18</td>
-        <td>Boris Horton</td>
-        <td>$35,094</td>
-        <td>Laos</td>
-        <td>Saint-Mard</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>19</td>
-        <td>Winifred Ryan</td>
-        <td>$64,436</td>
-        <td>Ireland</td>
-        <td>Ronciglione</td>
-        <td></td>
-      </tr>
+      <?php
+            $db = new database();
+            $sql ="SELECT * FROM users";
+            $data = $db-> display($data);
+            foreach($data as $row){
+                echo '<tr>';
+                    echo '<td>' . $row['UserID']        .'</td>';
+                    echo '<td>' . $row['Username']        .'</td>';
+                    echo '<td>' . $row['Email']        .'</td>';
+                    echo '<td>' . $row['UserRole']        .'</td>';
+                    echo '<td>' . $row['PhoneNumber']        .'</td>';
+                echo '</tr>';
+    }
+?>
     </tbody>
   </table>
 </div>
